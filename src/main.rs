e36b9892a207493539cc8d48c99dffd8151a3286
ClemensU42 <clemens_u@web.de>
2023-12-06 11:47:57 +0100

    added fltk and opened window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use fltk::{window::Window, app::App, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::default();
+    let mut wind = Window::new(100, 100, 100, 100, "ShadowTracker");
+    wind.end();
+    wind.show();
+    app.run().unwrap(); 
 }
